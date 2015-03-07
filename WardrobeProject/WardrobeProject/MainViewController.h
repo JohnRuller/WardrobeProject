98748194bca160bcfe52c15d0a4791815f3de12e
JohnRuller <john.ruller@gmail.com>
2015-03-07 13:05:57 -0600

    Second commit

---
+++ WardrobeProject/WardrobeProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  WardrobeProject
+//
+//  Created by John Ruller on 2015-03-07.
+//  Copyright (c) 2015 John Ruller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
