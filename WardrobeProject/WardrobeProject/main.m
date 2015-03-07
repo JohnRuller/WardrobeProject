0e55a72f2c49d99c7fd7ece1aed8056af5ccd1c6
JohnRuller <john.ruller@gmail.com>
2015-03-07 12:00:30 -0600

    First commit

---
+++ WardrobeProject/WardrobeProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WardrobeProject
+//
+//  Created by John Ruller on 2015-03-07.
+//  Copyright (c) 2015 John Ruller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
